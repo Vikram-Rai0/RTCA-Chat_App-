3ab4da76a08ed30cbb55f9cb513443d352e7d526
Vikram-Rai0 <177453319+Vikram-Rai0@users.noreply.github.com>
2026-08-30 22:46:25 +0545

    feat: add chat namespace

--- server/src/server.ts
+++ server/src/server.ts
@@ -17,7 +17,30 @@ const io = new Server(httpServer, {
   },
 });
 
-registerChatSocket(io);
+// Create /chat namespace
+const chat = io.of("/chat");
+
+chat.use((socket, next) => {
+  console.log("Chat middleware running");
+
+  next();
+});
+
+// chat.use((socket, next) => {
+//   const username = socket.handshake.auth.username;
+
+//   console.log("Middleware username:", username);
+
+//   if (!username) {
+//     return next(new Error("Username is required"));
+//   }
+
+//   socket.data.username = username;
+
+//   next();
+// });
+// Register chat events inside /chat
+registerChatSocket(chat);
 
 httpServer.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
